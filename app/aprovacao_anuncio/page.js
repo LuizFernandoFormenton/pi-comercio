1ecb2c845e46a19e3eec081ccac6d66488ad41ad
Gabriel997990622 <gabriel997990622@gmail.com>
2026-02-27 19:48:24 -0300

    criação das paginas: cadastro de amuncio e aprovação.

--- app/aprovacao_anuncio/page.js
+++ app/aprovacao_anuncio/page.js
@@ -1,8 +1,53 @@
-export default function TelaLogin() {
+export default function TelaDeAprovacao() {
   return (
     <div>
-     <h1> pagina de login </h1>
-      <h2>  </h2>
+     <h1 className="text-center"> Página de aprovação de anúncios </h1>
+      <br />
+      <br />
+      <p className="text-center"> <strong> aqui o anúncio sera aprovado ou recusado</strong></p>
+      <br />
+      <br />
+      <br />
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Data de Criação</th>
+            <th scope="col" className="text-center">Ação</th>
+          </tr>
+          <br/>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>anuncio 1</td>
+            <td>comida</td>
+            <td>01/01/1945</td>
+            <td className="text-center"><button type="button" className="btn btn-success">Aprovar</button>
+          <button type="button" className="btn btn-danger ms-2">Recusar</button></td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>anuncio 2</td>
+            <td>bebida</td>
+            <td>11/09/2000</td>
+            <td className="text-center"><button type="button" className="btn btn-success">Aprovar</button>
+          <button type="button" className="btn btn-danger ms-2">Recusar</button></td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>anuncio 3</td>
+            <td>materiais escolares</td>
+            <td>06/09/2024</td>
+            <td className="text-center"><button type="button" className="btn btn-success">Aprovar</button>
+          <button type="button" className="btn btn-danger ms-2">Recusar</button></td>
+          </tr>
+
+        </tbody>
+      </table>
 
     </div>
   );
--- app/tela_anuncio/page.js
+++ app/tela_anuncio/page.js
@@ -1,9 +1,40 @@
 
-export default function Home() {
-  return (
-    <div>
-     <h1> pagina inicial do projeto! </h1>
-      <h2> Cadastrar Anúncio </h2>
-    </div>
+export default function CadastroAnuncio() {
+return (
+
+  <div className="container mt-5"> 
+    <h1 className="mb-4">pagina de cadastro de anuncio!</h1>
+    
+    <form>
+      <div className="mb-3">
+        <label htmlFor="nomeAnuncio" className="form-label">Nome do Anúncio</label>
+        <input type="text" className="form-control" id="nomeAnuncio" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="descAnuncio" className="form-label">Descrição do Anúncio</label>
+        <input type="text" className="form-control" id="descAnuncio" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="imgAnuncio" className="form-label">Adicione a imagem do anúncio</label>
+        <input type="file" className="form-control" id="imgAnuncio" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="linkSite" className="form-label">Link do site</label>
+        <input type="url" className="form-control" id="linkSite" />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="linkWhatsApp" className="form-label">Link do site</label>
+        <input type="url" className="form-control" id="linkWhatsApp" />
+      </div>
+
+      <hr />
+      <button type="submit" className="btn btn-primary">Enviar</button>
+      <button type="button" className="btn btn-secondary ms-2">Cancelar</button>
+    </form>
+  </div>
   );
 }
