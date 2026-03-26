9c41399efef52a618429403c71c3607668ebeb31
Lariliki <larissa_ottoboni@hotmail.com>
2026-03-25 21:24:40 -0300

    atualização page inicial
    alterações

--- app/page.js
+++ app/page.js
@@ -4,47 +4,33 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import supabase from "./conexao/supabase";
 
-function Pagina_inicial({ params }) {
+function Pagina_inicial() {
 
   const [listaComercios, alteraListaComercios] = useState([])
   const [listaAnuncios, alteraListaAnuncios] = useState([])
-  const [listaCategorias, alteraListaCategorias] = useState([])
 
   async function buscarComercios() {
-    const { data, error } = await supabase
+    const { data} = await supabase
       .from('comercios')
       .select()
-
     alteraListaComercios(data)
   }
 
-  async function buscarCategorias() {
-    const { data, error } = await supabase
-      .from('comercios')
-      .select('*')
-      .eq('categoria', params.categoria)
-
-    alteraListaCategorias(data)
-  }
-
   async function buscarAnuncios() {
-    const { data, error } = await supabase
+    const { data} = await supabase
       .from('anuncios')
       .select('*')
-      console.error("Erro ao buscar anúncios:", error)
-
-      alteraListaAnuncios(data)
-    
+    alteraListaAnuncios(data)
   }
 
   useEffect(() => {
     buscarComercios();
-    buscarCategorias();
-    buscarAnuncios()
-
-  }, [params]);
+    buscarAnuncios();
+  }, []);
 
   return (
+
+
     <div className="container-fluid">
 
       {/* LINHA SUPERIOR */}
@@ -72,14 +58,11 @@ function Pagina_inicial({ params }) {
         </div>
       </div>
 
-
       <div className="row mt-4">
-        {/* CATEGORIAS - para fazer esse codigo só precisa de um eq*/}
+        {/* CATEGORIAS */}
         <div className="col-md-3 border-end">
           <h5 className="mb-3">Categorias</h5>
-          <ul className="list-group-item"
-            onClick={() => location.href = "/categoria/Restaurantes"}
-          >
+          <ul className="list-group">
             <li
               className="list-group-item"
               style={{ cursor: "pointer" }}
@@ -122,138 +105,63 @@ function Pagina_inicial({ params }) {
           </ul>
         </div>
 
-
-        {/* Cards vindos do banco */}
+        {/* CONTEÚDO */}
         <div className="col-md-9">
           <div className="row justify-content-center g-4">
-
-
-
-            {/*ANUNCIO*/}
-
-            <div className="row mt-4">
-              <div className="col-12">
-                <h5 className="mb-3">Anúncios</h5>
-
-                <div className="d-flex gap-3 overflow-auto pb-2">
-                  {listaAnuncios.map((item) => (
-                    <div>
-                      <img
-                        src={item.imagem}
-                        className="card-img-top"
-                        alt="Anúncio"
-                        style={{ height: "180px", objectFit: "cover" }}
-                      />
-
-                      <div className="card-body">
-                        <p className="card-text">{item.descricao}</p>
-
-                        <button
-                          className="btn btn-warning w-100"
-                          onClick={() => location.href = item.url}
-                        >
-                          Ver anúncio
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-
-            {listaComercios.map((item) => (
-              <div className="col-md-4 d-flex justify-content-center" key={item.id}>
-                <div className="card shadow" style={{ width: "22rem" }}>
-                  <img
-                    src={item.logo}
-                    className="card-img-top"
-                    alt={item.nome}
-                    style={{ height: "220px", objectFit: "cover" }}
-                  />
-
-                  <div className="card-body">
-                    <h5 className="card-title">{item.nome}</h5>
-                    <p className="card-text">{item.descricao}</p>
-                    <p className="card-text">📞 {item.telefone}</p>
-                    <p className="card-text">📍 {item.endereco}</p>
-                    <p className="card-text">🏷️ {item.categoria}</p>
-
-                    <button
-                      className="btn btn-warning w-100" onClick={() => location.href = "/comercio/" + item.id}>
-                      Ver mais 🔽
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-
-
-            {listaCategorias.map((item) => (
-              <div className="col-md-4 d-flex justify-content-center" key={item.id}>
-                <div className="card shadow" style={{ width: "22rem" }}>
-                  <img
-                    src={item.logo}
-                    className="card-img-top"
-                    alt={item.nome}
-                    style={{ height: "220px", objectFit: "cover" }}
-                  />
-
-                  <div className="card-body">
-                    <h5 className="card-title">{item.nome}</h5>
-                    <p className="card-text">{item.descricao}</p>
-                    <p className="card-text">📞 {item.telefone}</p>
-                    <p className="card-text">📍 {item.endereco}</p>
-                    <p className="card-text">🏷️ {item.categoria}</p>
-
-                    <button
-                      className="btn btn-warning w-100" onClick={() => location.href = "/comercio/" + item.id}>
-                      Ver mais 🔽
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
+            
 
             {/* ANÚNCIOS */}
-            <div className="row mt-4">
-              <div className="col-12">
-                <h5 className="mb-3">🔥 Anúncios em destaque</h5>
 
-                <div className="d-flex gap-3 overflow-auto pb-2">
-
-                  {listaAnuncios.map((item) => (
-                    <div>
-                      <img
-                        src={item.imagem}
-                        className="card-img-top"
-                        alt="Anúncio"
-                        style={{ height: "160px", objectFit: "cover" }}
-                      />
-
-                      <div className="card-body">
-                        <p className="card-text">{item.descricao}</p>
-
-                        <button
-                          className="btn btn-warning w-100"
-                          onClick={() => location.href = "/anuncio/" + item.id}
-                        >
-                          Ver oferta 🔥
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-
-                </div>
+            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+              <div className="carousel-inner">
+                {listaAnuncios.map((item) => (
+                  <div
+                    className={"carousel-item active"}
+                  >
+                    <img
+                      src={item.imagem}
+                      className="d-block w-70"
+                      alt="Anúncio"
+                      onClick={() => location.href = item.url}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
+
+            {/* COMÉRCIOS */}
+            {listaComercios.map((item) => (
+              <div className="col-md-4 d-flex justify-content-center">
+                <div className="card shadow" style={{ width: "22rem" }}>
+                  <img
+                    src={item.logo}
+                    className="card-img-top"
+                    alt={item.nome}
+                    style={{ height: "220px", objectFit: "cover" }}
+                  />
+
+                  <div className="card-body">
+                    <h5 className="card-title">{item.nome}</h5>
+                    <p className="card-text">{item.descricao}</p>
+                    <p className="card-text">📞 {item.telefone}</p>
+                    <p className="card-text">📍 {item.endereco}</p>
+                    <p className="card-text">🏷️ {item.categoria}</p>
+
+                    <button
+                      className="btn btn-warning w-100"
+                      onClick={() => location.href = "/comercio/" + item.id_comercio}
+                    >
+                      Ver mais 🔽
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-
       </div>
     </div>
   );
 }
-
-
 
 export default Pagina_inicial;
